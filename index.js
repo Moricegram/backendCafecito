8ba9bf6215b73fb829430b7a534bfd82b1db459d
Mauricio Aguirre <ing.mauricio.aguirre@gmail.com>
2023-08-23 21:22:35 -0300

    Agregamos una variable para escuchar el puerto

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express';
-console.log('esto es una prueba')
 // 1- configuraciones iniciales
-
+const app = express();
+//crear una variable con express
+app.set('port', process.env.PORT || 4000)
 
 // 2- middlewares (permite solicitudes externas)
 
